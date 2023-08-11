a80b83a57a6eac740a42f92d62cba6ef0d00cecf
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-08-11 15:37:27 +1000

    setting up project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      {/* Spotify logo */}
+      {/* Login with Spotify button */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Login.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Login.css";
+
+const Login = () => {
+  return (
+    <div>
+      {/* spotify  */}
+      {/* login with spotify */}
+    </div>
+  );
+};
+
+export default Login;
