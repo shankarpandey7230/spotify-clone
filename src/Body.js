8578c132a15fd12336a5077612177ae1e5f0c962
shankarpandey7230 <shankarpandey7230@gmail.com>
2023-08-14 19:11:19 +1000

    Material icons and crearting playlist

--- src/Body.js
+++ src/Body.js
@@ -1,7 +1,12 @@
 import React from "react";
+import "./Body.css";
 
 const Body = () => {
-  return <div></div>;
+  return (
+    <div className="body">
+      <h1> I am the body</h1>
+    </div>
+  );
 };
 
 export default Body;
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,32 @@
 import React from "react";
+import "./Sidebar.css";
+import SidebarOptions from "./SidebarOptions";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import LibraryMusicOutlinedIcon from "@mui/icons-material/LibraryMusicOutlined";
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <img
+        className="sidebar_logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+        alt=""
+      />
+      <SidebarOptions Icon={HomeOutlinedIcon} title="Home" />
+      <SidebarOptions Icon={SearchOutlinedIcon} title="Search" />
+      <SidebarOptions Icon={LibraryMusicOutlinedIcon} title="Your Library" />
+      <br />
+      <strong className="sidebar_title">PLAYLISTS</strong>
+      <hr />
+
+      <SidebarOptions title="Hiphop" />
+
+      <SidebarOptions title="Rock" />
+
+      <SidebarOptions title="Dohori" />
+    </div>
+  );
 };
 
 export default Sidebar;
